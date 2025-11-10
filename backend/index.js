1924b5dbdfde23fd9b4754dbe132e8c4409fa621
nikhilahluwalia <nikhil_ahluwalia@hotmail.com>
2025-11-10 10:53:42 +0530

    Add dotenv configuration and update dependencies in package.json

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+
 const app = express();
 const PORT = 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
